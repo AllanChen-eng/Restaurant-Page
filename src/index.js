331bbb8c8c6e8aa1ce76f6312d813d909bf7edf2
Allan C <59120947+AllanChen-eng@users.noreply.github.com>
2025-08-09 13:21:51 -0700

    Initial layout for home page and clear button
    Clear button will allow users to clear the page
    without adding more content.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log("Hello World!");
+import "./styles.css";
+console.log("hello World");
+const content = document.querySelector(".content");
+
+function createHome(){
+
+}
+function createMenu(){
+
+}
+function createAbout(){
+
+}
+function clearContents(){
+    const clearBtn = document.querySelector("#clear-btn");
+    const removeContent = () =>{
+        content.innerHTML="";
+    }
+    clearBtn.addEventListener("click", ()=>{
+        removeContent();
+    });
+}
+clearContents();
